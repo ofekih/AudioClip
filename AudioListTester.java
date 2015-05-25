fe24d02c5b688ded1574cf44030c84031bd79f74
Ofek Gila <flamingo.ofek@gmail.com>
2015-05-25 12:36:06 -0700

    Search types
    can search everything, current folder, or folder and subfolders

--- AudioListTester.java
+++ AudioListTester.java
@@ -1,6 +1,7 @@
 public class AudioListTester {
 	public static void main(String[] args) {
-		AudioList al = new AudioList(true);
+		AudioList al = new AudioList(AudioList.INITIAL_SHUFFLE, AudioList.SEARCH_EVERYTHING);
+		al.setRate(1);
 		al.play();
 	}
 }
